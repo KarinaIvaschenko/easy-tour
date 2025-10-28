b80d8ed794658653b6172f5a5d5ec94563e5f0e2
KarinaIvaschenko <karinka2323@gmail.com>
2025-10-28 22:49:10 +0200

    init project

---
+++ README.md
@@ -0,0 +1,19 @@
+# EasyTour
+
+Package name: easy-tour
+
+## Only use Yarn commands
+
+Node version: >=22.0.0
+
+## Install node_modules
+
+yarn
+
+## Runs the app in the development mode
+
+yarn start
+
+## Builds the app for production to the `build` folder
+
+yarn build
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Route, Routes} from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage.tsx";
+
+function App() {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
